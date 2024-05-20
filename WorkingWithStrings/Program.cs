9edf731c2d37b5c32b48ebe7575dd408dd9f8587
Adjanour <adjanour@icloud.com>
2024-05-20 22:44:45 +0000

    added files

---
+++ WorkingWithStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace WorkingWithStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    string myString = "My \"so - called\" life ";
+            //    string myStrings = @"My ""life"" ";
+            //    string my = "What if I need a\n new line?";
+            //    string directory = @"Go To your c:\drive";
+            //    Console.Write(myString);
+            //    string myStringx = String.Format("{0:C},{1}", 123.45, myString);
+            //    Console.Write(myStringx);
+            //    Console.WriteLine("Hello, World!");'
+
+
+            // Manipulating Date and Time
+            DateTime myValue = DateTime.Now;
+            Console.WriteLine(myValue.ToString());
+            Console.WriteLine(myValue.Month);
+          
+        }
+    }
+}
